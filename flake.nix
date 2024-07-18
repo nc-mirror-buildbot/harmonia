36203aef6802ab308e12b518ecfb45bfe91773c8
test <test@example.com>
2024-07-18 17:17:43 +0200

    test

--- flake.nix
+++ flake.nix
@@ -44,7 +44,7 @@
               t01-signing = import ./tests/t01-signing.nix testArgs;
               t02-varnish = import ./tests/t02-varnish.nix testArgs;
               t03-chroot = import ./tests/t03-chroot.nix testArgs;
-              test-package = pkgs.writeText "test-package" (toString 3);
+              test-package = pkgs.writeText "test-package" (toString 4);
             } // {
             clippy = config.packages.harmonia.override ({
               enableClippy = true;
